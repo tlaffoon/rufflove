b64ef595b2897ac986cf7c4d978be54d11e1168a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-07-14 12:58:51 -0500

    Added additional functions, prepping for user integration.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,4 +20,69 @@ class HomeController extends BaseController {
 		return View::make('hello');
 	}
 
+	public function showHome()
+	{
+		return View::make('homepage');
+	}
+
+	public function showLogin()
+	{
+		return View::make('login');
+	}
+
+	public function doLogin() 
+	{
+
+		$rules = array(
+			'email'    => 'required|email', // make sure the email is an actual email
+			'password' => 'required|alphaNum|min:6' // password can only be alphanumeric and has to be greater than 6 characters
+		);
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if ($validator->fails()) {
+			return Redirect::to('login')
+				->withErrors($validator) // send back all errors to the login form
+				->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
+		} 
+
+		else 
+		{
+
+			// create our user data for the authentication
+			$userdata = array(
+				'email' 	=> Input::get('email'),
+				'password' 	=> Input::get('password')
+			);
+
+			if (Auth::attempt($userdata)) 
+			{
+		    	return Redirect::intended('/posts');
+			}
+			
+			else
+			{
+				Session::flash('errorMessage', 'There were errors submitting your form.  Did you include all fields?');
+				return Redirect::to('login')->withInput()->withErrors($validator);;
+			}
+		}
+	}
+
+	public function doLogout()
+	{
+		Auth::logout(); // log the user out of our application
+		return Redirect::action('HomeController@showHome'); // redirect the user to the homepage
+	}
+
+
+	public function showResume()
+	{
+		return View::make('resume');
+	}
+
+	public function showPortfolio() 
+	{
+		return View::make('portfolio');
+	}
+
 }
