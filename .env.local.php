c2e7f8814a71b131b3cb5599befb00afeda97116
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-07-14 14:27:15 -0500

    Added environment files.

---
+++ .env.local.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'DB_NAME' => 'ruff_db',
+    'DB_USER' => 'ruffian',
+    'DB_PASS' => 's3Ucumen',
+
+    'ADMIN_NAME' => 'ruffluvr',
+    'ADMIN_PASS' => 'wuNabR3s'
+);
