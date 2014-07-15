cf21289fc227c5bceda64b247ffeaeb9c1ed0c86
Genaro Garza <github.rf9@gishpuppy.com>
2014-07-15 10:07:14 -0500

    updated seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UserTableSeeder');
-		// $this->call('UserTableSeeder');
+		$this->call('DogTableSeeder');
 	}
 }
 class UserTableSeeder extends Seeder {
@@ -21,28 +21,44 @@ class UserTableSeeder extends Seeder {
     {
         DB::table('users')->delete();
 
-        $user = new User();
-        
-        
-        $user->save();
-
         for ($i=1; $i <= 10; $i++) 
         { 
 	        $user = new User();
 	        $user->first_name = "John" . $i;
 	        $user->last_name = "Doe" . $i;
-	        $user->address =  $i . "Acacia Avenue.";
+	        $user->address =  $i . " Acacia Avenue.";
 	        $user->city = "San Antonio";
 	        $user->state = "TX";
 	        $user->zip = "78213";
 	        $user->username = "doglover" . $i;
 	        $user->password = "password";
-	        $user->email = $_ENV['ADMIN_USER'];
-	        $user->image_path = "/img-upload/dog_picture.jpg";
+	        $user->email = "doglover@rufflove.dev";
+	        $dog->image_path = $imgDir . "/owners/picture.jpg";
 	        $user->save();
-        }
+        } // end for loop
+	} //end method run()
+}  //end class
+ 
 		
-    }
-}
+class DogTableSeeder extends Seeder {
+
+	public function run()
+	{
+        DB::table('dogs')->delete();
+
+        for ($i=1; $i <= 10; $i++) 
+        { 
+	        $dog = new Dog();
+	        $dog->name = "Fido " . $i;
+	        $dog->breed = "Doberman Pincher";
+	        $dog->purebreed = "TRUE";
+	        $dog->age = $i;
+	        $dog->weight = "80";
+	        $dog->sex = "M";
+	        $dog->image_path = $imgDir . "/dogs/picture.jpg";	       
+	       	$dog->save();
+        } // end for loop
+	} //end run()
+} // end class DogTableSeeder
 
 
