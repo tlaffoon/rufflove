4ed954e9a3af14ec0c99d2539ae0a2d9c373cb8d
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-07-21 06:16:06 -0500

    Refactored to include autocomplete function, and geocode updates lat/lng in form.

--- app/views/map.blade.php
+++ app/views/map.blade.php
@@ -7,22 +7,29 @@
   body { height: 100%; margin: 0; padding: 0 }
   #map-canvas { height: 100% }
 
-  .width80 {
-    width: 80%;
-  }
-
   #addr-btn {
-    float: right;
+/*    float: right;
     position: relative;
     top: -53px;
-    left: -20px;
+    left: -20px;*/
   }
 
 </style>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3Q4UzAfKDUWGr-KbI3nj19LxBstHbRZY"></script>
-
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+<!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3Q4UzAfKDUWGr-KbI3nj19LxBstHbRZY"></script>
+ -->
 <script type="text/javascript">
     var map;
+    var placeSearch, autocomplete;
+    var componentForm = {
+      street_number: 'short_name',
+      route: 'long_name',
+      locality: 'long_name',
+      administrative_area_level_1: 'short_name',
+      country: 'long_name',
+      postal_code: 'short_name'
+    };
+
     function initialize() {
       var mapOptions = {
         zoom: 10,
@@ -30,15 +37,39 @@
       };
       map = new google.maps.Map(document.getElementById('map-canvas'),
           mapOptions);
+
+    // Add autocomplete to input form
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
+        { types: ['geocode'] });
+    // When the user selects an address from the dropdown,
+    // populate the address fields in the form.
+    //google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      //fillInAddress();
+    //});
     }
-    
-    // Define input field for autocomplete option
-    //var input = document.getElementById('addr-value');
 
-    // Call autocomplete function on defined input field
-    //var autocomplete = new google.maps.places.Autocomplete(input);
+    // [START region_fillform]
+    function fillInAddress() {
+      // Get the place details from the autocomplete object.
+      var place = autocomplete.getPlace();
 
-    //autocomplete.bindTo('bounds', map);
+      for (var component in componentForm) {
+        document.getElementById(component).value = '';
+        document.getElementById(component).disabled = false;
+      }
+
+      // Get each component of the address from the place details
+      // and fill the corresponding field on the form.
+      for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+          var val = place.address_components[i][componentForm[addressType]];
+          document.getElementById(addressType).value = val;
+        }
+      }
+    }
+    // [END region_fillform]
 
     // Initialize map
     google.maps.event.addDomListener(window, 'load', initialize);
@@ -48,20 +79,40 @@
 @section('content')
 
 <div class="col-md-5">
+
+
     <div class="page-header">
         <h2> Google Maps Integration </h2>
     </div>
       <h3>Please enter address:</h3>
-            <input type="text" id="addr-value" name="address" class="form-control form-group width80" value="339 Mahogany Chest, San Antonio, TX, 78249" placeholder="">
-            <button id="addr-btn" class="btn btn-default" action="submit">Submit</button>
+        <!-- <form> -->
+            <input type="text" id="autocomplete" name="address" class="form-control form-group" value="" placeholder="">
+            <button id="addr-btn" class="btn btn-default pull-right" action="submit">Submit</button>
+        <!-- </form> -->
     <br>
-
-    <p id="address-text"></p> <!-- Display address upon user input and button click -->
-    <div id="ajax-message"></div>
+    
+    <div class="col-md-3 zero-pad-left">
+        <form>
+            <label>Latitude</label>
+            <input id="latitude" name="latitude" class="form-group form-control" placeholder="" value="">
+            <label>Longitude</label>
+            <input id="longitude" name="longitude" class="form-group form-control" placeholder="" value="">
+    </div>
+    <div class="col-md-7 zero-pad-left">
+            <label>Street</label>
+            <input id="street" name="street" class="form-group form-control" placeholder="" value="">
+            <label>City</label>
+            <input id="city" name="city" class="form-group form-control" placeholder="" value="">
+            <label>State</label>
+            <input id="state" name="state" class="form-group form-control" placeholder="" value="">
+            <label>Zip</label>
+            <input id="zip" name="zip" class="form-group form-control" placeholder="" value="">
+        </form>
+    </div>
 
 </div> <!-- end left container -->
 
-<div class="col-md-7">
+<div class="col-md-7"> <!-- begin right container -->
     <div class="page-header text-right">
         <h2>Sample Map</h2>
     </div>
@@ -75,73 +126,42 @@
 @section('bottomscript')
 <script type="text/javascript">
 
-// var markers = [];
-var markers = new Array();
-
-// var addresses = [];
-var addresses = new Array();
-
-function LoopForever() {
-
-    addresses.forEach(function (element, index) {
-        // console.log(element);
-        // $('#address-text').html('You added: ' + address);
-
-    });
-
-} // end forever loop
-
+    // Add Marker on Address Submit and Button Click
     $('#addr-btn').click(function () {
+        // Prevent default behavior of form submit
+        // e.preventDefault();
         // Set variable address equal to user input
-        var address = $('#addr-value').val();
-        // Add address to array of addresses
-        addresses.push(address);
-
-        // Update address-text with address value
-        // $('#address-text').text('You added: ' + address);
+        var address = $('#autocomplete').val();
 
         // Geocode address to get lat/lng
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({ 'address': address }, function(result, status) {
             
+            // If geocode status is OK, then set lat/lng variables.
             if (status == google.maps.GeocoderStatus.OK) {
               var latLngObj = result[0]["geometry"]["location"];
+
+              // Update fields above with lat/lng values
+              $('#latitude').val(result[0]["geometry"]["location"]["k"]);  // need to call functions instead of these variables
+              $("#longitude").val(result[0]["geometry"]["location"]["B"]); //  ^
             }
 
-        // Create new marker based on lat/lng
-        var marker = new google.maps.Marker({
-            position: latLngObj,
-            map: map,
-            draggable: false,
-            title: "Hello World!"
-            //animation: google.maps.Animation.DROP,
-        });  // End Marker
-
-            // Store marker in array to keep all markers on the page, and allow easy reset
-            markers.push(marker);
-
+            // Create new marker based on lat/lng
+            var marker = new google.maps.Marker({
+                position: latLngObj,
+                map: map,
+                draggable: false,
+                title: "Marker"
+                // animation: google.maps.Animation.DROP,
+            });  // End Marker
         }); // End Geocoder
 
-        // Begin Ajax Request
-            // Define address addrData json object with address
-            var addrData = {
-                'address': address,
-            };
+        // Populate Address Form with data parsed from autocomplete
+        // ... Street
+        // ... City
+        // ... State
+        // ... Zip
 
-
-            // Log it
-            // console.log('addrData: ' + addrData);
-
-            // Send ajax request via route, which returns a response of json data
-            $.ajax({
-                url: "/map",
-                type: "POST",
-                data: addrData,
-                dataType: "json",
-                success: function (data) {
-                    $('#ajax-message').html(data.message);
-                }
-            });
     }); // End click event for addr-btn
 
 </script>
