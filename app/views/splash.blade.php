ec2539b220b9d0cee3453bb8c341707433a996ee
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-07-31 23:36:18 -0500

    Altered to use new mailchimp account.

--- app/views/splash.blade.php
+++ app/views/splash.blade.php
@@ -31,8 +31,7 @@
 	 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 	 </style>
 	 <div id="mc_embed_signup" class="img-rounded" style="width: 95%;">
-	 <form action="//khemitology.us7.list-manage.com/subscribe/post?u=238d10a4d43535c14ac289459&amp;id=33d4b59d58" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form form-inline" target="_blank" novalidate>
-	 	<label for="mce-EMAIL"></label>
+<form action="//ruff-love.us8.list-manage.com/subscribe/post?u=931944f7e4688d340ee1dd4d5&amp;id=62b323e522" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>	 	<label for="mce-EMAIL"></label>
 	 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email.address@domain.com" style="font-color: black;"required>
 	     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 	     <div style="position: absolute; left: -5000px;"><input type="text" name="b_238d10a4d43535c14ac289459_33d4b59d58" tabindex="-1" value=""></div>
